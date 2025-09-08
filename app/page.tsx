fca29ef7cf26e3406f59fbe61bd3ccca785f00eb
Alex Carpenter <im.alexcarpenter@gmail.com>
2025-09-08 09:01:35 -0400

    init

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main className="w-full h-dvh grid place-content-center">
+      <h1 className="text-3xl">
+        Resilient{" "}
+        <span className="p-2 inline-grid place-content-center bg-neutral-700 rounded-md">
+          UI
+        </span>
+      </h1>
+    </main>
+  );
+}
