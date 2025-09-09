eb1b6c2215c97bb9bbc7a6ee8116b6ee45cb1791
Alex Carpenter <im.alexcarpenter@gmail.com>
2025-09-09 19:10:55 -0400

    fix

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next"
-import { createMDX } from "fumadocs-mdx/next"
-
-const withMDX = createMDX()
-
-const nextConfig: NextConfig = {}
-
-export default withMDX(nextConfig)
