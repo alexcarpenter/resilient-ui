ac786be49c373f64cf29c1f6cbb336f626674a54
Alex Carpenter <im.alexcarpenter@gmail.com>
2025-09-09 18:09:16 -0400

    ignore source dir

--- .source/index.ts
+++ .source/index.ts
@@ -1,7 +1,7 @@
 // @ts-nocheck -- skip type checking
-import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=1757455107211"
-import * as changelog_0 from "../content/changelog/example.mdx?collection=changelog&hash=1757455107211"
-import * as blog_0 from "../content/blog/example.mdx?collection=blog&hash=1757455107211"
+import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=1757455391779"
+import * as changelog_0 from "../content/changelog/example.mdx?collection=changelog&hash=1757455391779"
+import * as blog_0 from "../content/blog/example.mdx?collection=blog&hash=1757455391779"
 import { _runtime } from "fumadocs-mdx"
 import * as _source from "../source.config"
 export const blog = _runtime.doc<typeof _source.blog>([{ info: {"path":"example.mdx","absolutePath":"/Users/alexcarpenter/repos/resilient-ui/content/blog/example.mdx"}, data: blog_0 }]);
