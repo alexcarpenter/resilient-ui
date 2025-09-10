8316af416d728d99e9c601e0ee0ac6cddfa091a2
Alex Carpenter <alex.carpenter@clerk.dev>
2025-09-09 20:50:52 -0400

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 import { createMDX } from "fumadocs-mdx/next"
 
-const withMDX = createMDX({
-  configPath: "source.config.ts",
-})
+const withMDX = createMDX()
 
 /** @type {import('next').NextConfig} */
 const config = {
