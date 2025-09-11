b298ded24582f2cdc1f98619da1ebf43c7f3c669
Alex Carpenter <im.alexcarpenter@gmail.com>
2025-09-11 08:12:58 -0400

    add base-ui tabs

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -1,19 +1,24 @@
 import React from "react"
+import { Tabs } from "@base-ui-components/react/tabs"
 
-export function CodeBlockTabs(props: React.ComponentProps<"div">) {
-  return <div {...props} />
+export function CodeBlockTabs(props: React.ComponentProps<typeof Tabs.Root>) {
+  return <Tabs.Root {...props} />
 }
 
-export function CodeBlockTabsList(props: React.ComponentProps<"div">) {
-  return <div {...props} />
+export function CodeBlockTabsList(
+  props: React.ComponentProps<typeof Tabs.List>
+) {
+  return <Tabs.List {...props} />
 }
 
-export function CodeBlockTabsTrigger(props: React.ComponentProps<"button">) {
-  return <button {...props} />
+export function CodeBlockTabsTrigger(
+  props: React.ComponentProps<typeof Tabs.Tab>
+) {
+  return <Tabs.Tab {...props} />
 }
 
-export function CodeBlockTab(props: React.ComponentProps<"div">) {
-  return <div {...props} />
+export function CodeBlockTab(props: React.ComponentProps<typeof Tabs.Panel>) {
+  return <Tabs.Panel {...props} />
 }
 
 export const mdxComponents = {
