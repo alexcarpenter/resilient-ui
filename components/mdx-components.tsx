2a715c751d909583d86f5b0da3ee396b87b9546a
Alex Carpenter <im.alexcarpenter@gmail.com>
2025-09-11 08:10:34 -0400

    init

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -1,1 +1,24 @@
-export const mdxComponents = {};
+import React from "react"
+
+export function CodeBlockTabs(props: React.ComponentProps<"div">) {
+  return <div {...props} />
+}
+
+export function CodeBlockTabsList(props: React.ComponentProps<"div">) {
+  return <div {...props} />
+}
+
+export function CodeBlockTabsTrigger(props: React.ComponentProps<"button">) {
+  return <button {...props} />
+}
+
+export function CodeBlockTab(props: React.ComponentProps<"div">) {
+  return <div {...props} />
+}
+
+export const mdxComponents = {
+  CodeBlockTabs,
+  CodeBlockTabsList,
+  CodeBlockTabsTrigger,
+  CodeBlockTab,
+}
