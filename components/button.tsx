3e3ab01c3f4c138f06573f291150cefd1972eeb4
Alex Carpenter <im.alexcarpenter@gmail.com>
2025-09-25 08:26:08 -0400

    wip

--- components/button.tsx
+++ components/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-md gap-x-2 text-sm",
+  "inline-flex items-center justify-center rounded-md gap-x-2 text-sm [font-weight:425]",
   {
     variants: {
       variant: {
