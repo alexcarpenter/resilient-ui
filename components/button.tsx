dc61227cf8cc9d6c8457c693039a1bec054f5d79
Alex Carpenter <im.alexcarpenter@gmail.com>
2025-09-25 08:23:12 -0400

    wip

--- components/button.tsx
+++ components/button.tsx
@@ -4,21 +4,25 @@ import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
 
-const buttonVariants = cva("inline-flex items-center gap-x-2", {
-  variants: {
-    variant: {
-      default: "bg-red-500",
+const buttonVariants = cva(
+  "inline-flex items-center justify-center rounded-md gap-x-2 text-sm",
+  {
+    variants: {
+      variant: {
+        default: "bg-neutral-800 text-neutral-50",
+        secondary: "bg-neutral-50 text-neutral-950",
+      },
+      size: {
+        default: "h-10 px-4 py-2",
+        icon: "size-10",
+      },
     },
-    size: {
-      default: "h-10 px-4 py-2",
-      icon: "size-10",
+    defaultVariants: {
+      variant: "default",
+      size: "default",
     },
-  },
-  defaultVariants: {
-    variant: "default",
-    size: "default",
-  },
-})
+  }
+)
 
 interface ButtonProps
   extends VariantProps<typeof buttonVariants>,
