28d502925b5fb9fadaf50e150dc24f7b3ef05ba6
Alex Carpenter <im.alexcarpenter@gmail.com>
2025-11-25 07:53:40 -0500

    add GitHubButton

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,49 @@
 import { siteConfig } from "@/lib/config"
+// import { GitHubButton } from "@/components/github-button"
 
 export default function Home() {
   return (
-    <main className="grid w-full flex-1 place-content-center px-4 py-16">
-      <div className="flex flex-col gap-y-6">
-        <div className="flex flex-col gap-y-1">
-          <p className="text-muted-foreground flex items-center justify-between font-mono text-xs">
-            <span>v{siteConfig.version}</span>
-            <a
-              href={siteConfig.links.github}
-              target="_blank"
-              rel="noopener"
-              className="hover:text-foreground transition-colors"
+    <>
+      <main className="grid w-full flex-1 place-content-center px-4 py-16">
+        <div className="flex flex-col gap-y-6">
+          <div className="flex flex-col gap-y-1">
+            <p className="text-muted-foreground flex items-center justify-between font-mono text-xs">
+              <span>v{siteConfig.version}</span>
+              <a
+                href={siteConfig.links.github}
+                target="_blank"
+                rel="noopener"
+                className="hover:text-foreground transition-colors"
+              >
+                {process.env.VERCEL_GIT_COMMIT_SHA?.slice(0, 7)}
+              </a>
+            </p>
+            <h1
+              className="text-xl uppercase md:text-3xl"
+              style={{
+                fontStretch: "200%",
+                fontWeight: "900",
+              }}
             >
-              {process.env.VERCEL_GIT_COMMIT_SHA?.slice(0, 7)}
-            </a>
-          </p>
-          <h1
-            className="text-xl uppercase md:text-3xl"
-            style={{
-              fontStretch: "200%",
-              fontWeight: "900",
-            }}
-          >
-            Resilient&mdash;UI
-          </h1>
-          <p className="text-muted-foreground text-right font-mono text-xs uppercase">
-            by{" "}
-            <a
-              href="https://alexcarpenter.me"
-              target="_blank"
-              rel="noopener"
-              className="hover:text-foreground transition-colors"
-            >
-              Alex Carpenter
-            </a>
-          </p>
+              Resilient&mdash;UI
+            </h1>
+            <p className="text-muted-foreground text-right font-mono text-xs uppercase">
+              by{" "}
+              <a
+                href="https://alexcarpenter.me"
+                target="_blank"
+                rel="noopener"
+                className="hover:text-foreground transition-colors"
+              >
+                Alex Carpenter
+              </a>
+            </p>
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+      {/* <footer className="flex justify-center px-4 py-16">
+        <GitHubButton />
+      </footer> */}
+    </>
   )
 }
